754d17e61939d51ace01cfc5218ea980c4c89d7e
mybodya <mybodya@gmail.com>
2013-09-03 15:04:21 +0200

    making small and clear demo

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,22 @@
-//we are getting rid of "sample extension" and starting with real working one
+;(function() {
+
+    'use strict';
+
+    // lines above are for incapsulation purposes only
+
+    // this function is to prevent data be executed as HTML (unused now)
+    /*function htmlEntities(str) {
+        return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    }*/
+
+    var serviceHost = 'http://example.com',
+        serviceQuery = ':8080/api/?query=';
+
+    $.ajax(serviceHost + serviceQuery + encodeURIComponent(location.href), {
+        dataType: 'json',
+        complete: function (json) {
+            console.log(json);
+        }
+    });
+
+})();
