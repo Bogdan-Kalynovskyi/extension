4fe607e3c4e06043c06eaaaa14b74df8eb2b4d24
mybodya <mybodya@gmail.com>
2013-09-03 15:33:42 +0200

    important bugfixes, read todos

--- extension/content.js
+++ extension/content.js
@@ -41,6 +41,8 @@
     var serviceHost = 'http://example.com',
         serviceQuery = ':8080/api/?query=';
 
+    console.log(location.href);
+
     $.ajax(serviceHost + serviceQuery + encodeURIComponent(location.href), {
         dataType: 'json',
         complete: function (json) {
