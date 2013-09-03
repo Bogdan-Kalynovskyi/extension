4c525d354df5cfb597af86ada54b7573ce907451
mybodya <mybodya@gmail.com>
2013-09-03 15:04:21 +0200

    making small and clear demo

--- extension/content.js
+++ extension/content.js
@@ -2,6 +2,7 @@
 
     'use strict';
 
+<<<<<<< HEAD
     var popup,
         css = '_lj_popup' + Date.now();
 
@@ -87,11 +88,23 @@
             }
         }
     });
+=======
+    // lines above are for incapsulation purposes only
 
+    // this function is to prevent data be executed as HTML (unused now)
+    /*function htmlEntities(str) {
+        return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    }*/
 
-    document.body.addEventListener('mousedown', function (e) {
-        if (popup && popup !== e.target) {
-            removePopup();
+>>>>>>> making small and clear demo
+
+    var serviceHost = 'http://example.com',
+        serviceQuery = ':8080/api/?query=';
+
+    $.ajax(serviceHost + serviceQuery + encodeURIComponent(location.href), {
+        dataType: 'json',
+        complete: function (json) {
+            console.log(json);
         }
     });
 
