6767d68b1f8a8367330c94db7b660145f0532f1d
mybodya <mybodya@gmail.com>
2013-10-28 14:05:39 +0100

    some change

--- extension/content.js
+++ extension/content.js
@@ -5,15 +5,15 @@
     // lines above are for incapsulation purposes only
 
 
-    var serviceHost = 'http://nearbyfuture.com',
-        serviceQuery = ':8080/api/?query=';
+    var serviceURL = 'http://nearbyfuture.com:8080/api/?query=';
 
-    console.log(location.href);
-
-    $.ajax(serviceHost + serviceQuery + encodeURIComponent(location.href), {
+    $.ajax(serviceURL + encodeURIComponent(location.href), {
         dataType: 'json',
         complete: function (json) {
             console.log(json);
+        },
+        error: function () {
+
         }
     });
 
