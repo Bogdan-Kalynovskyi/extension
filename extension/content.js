bf5436ddbff4dc388591e66015bfd04bcc32831f
mybodya <mybodya@gmail.com>
2013-10-28 14:05:39 +0100

    some change

--- extension/content.js
+++ extension/content.js
@@ -41,12 +41,13 @@
     var serviceHost = 'http://example.com',
         serviceQuery = ':8080/api/?query=';
 
-    console.log(location.href);
-
-    $.ajax(serviceHost + serviceQuery + encodeURIComponent(location.href), {
+    $.ajax(serviceURL + encodeURIComponent(location.href), {
         dataType: 'json',
         complete: function (json) {
             console.log(json);
+        },
+        error: function () {
+
         }
     });
 
