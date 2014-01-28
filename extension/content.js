f9dfc9d7d533f2599d18540f1f45fdab101b0af4
easy-one <mybodya@gmail.com>
2014-01-28 01:43:30 +0100

    frontend: creating prototype for rating system

--- extension/content.js
+++ extension/content.js
@@ -2,19 +2,73 @@
 
     'use strict';
 
-    // lines above are for incapsulation purposes only
+	var host = 'http://192.168.1.72:8080/api/rating/',
+		userName = getLoggedUserId(),
+		author = location.host.substr(0, location.host.indexOf('.')),
+		postId = parseInt(location.pathname.substr(1), 10);
 
 
-    var serviceURL = 'http://nearbyfuture.com:8080/api/?query=';
 
-    $.ajax(serviceURL + encodeURIComponent(location.href), {
-        dataType: 'json',
-        complete: function (json) {
-            console.log(json);
-        },
-        error: function () {
+    function getLoggedUserId() {
+    	var hiddenInput = document.querySelector('#Greeting input[name=user]');
 
-        }
+    	if (hiddenInput) {
+    		return hiddenInput.value;
+    	} else {
+    		return null;
+    	}
+    }
+
+
+	function doRate(event) {
+		var el = this,
+			url = host + 'set/',
+			commentId = parseInt(el.parentNode.id.substr(1)),
+			elementData = el.getBoundingClientRect(),
+			width = elementData.width,
+			clickX = event.pageX - elementData.left,
+			rating = (clickX - width/2) / width/2 * 100,
+			data = {
+				userName: userName,
+				author: author,
+				postId: postId,
+				commentId: commentId,
+				rating: rating
+			};
+
+		ajax.get(url, data, function() {
+			//
+		});
+	}
+
+
+    function createRatingBoxes() {
+    	var commentHeaderCssQuery = '.commentHeader',	// we inject rating bar in comment header
+    		allHeaders = document.querySelectorAll(commentHeaderCssQuery),
+    		ratingBarPrototype = document.createDocumentFragment(),
+    		clone,
+    		header,
+    		commentId;
+
+		ratingBarPrototype.innerHTML =  '<div class="rating"></div>';
+
+		for (var i = 0, l = allHeaders.length; i < l; i++) {
+			clone = ratingBarPrototype.cloneNode(true);
+			clone.childNodes[0].addEventListener('click', doRate);
+
+			header = allHeaders[i];
+			commentId = parseInt(header.parentNode.id.substr(1)),
+			header.appendChild(clone);
+		}
+
+    }
+
+
+    ajax.get(host, {
+	    	author: author,
+	    	postId: postId
+	    }, function() {
+	    	//
     });
 
 })();
