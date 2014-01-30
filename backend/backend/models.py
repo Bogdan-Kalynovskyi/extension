da819f26432820a220e9fc5c9abf23b8b8e45379
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-01-30 13:54:22 +0200

    Make working version

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -3,9 +3,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Post(models.Model):
+    author = models.ForeignKey(User)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    parent = models.ForeignKey('self', related_name='children', null=True)
+    
+
 class CommentRating(models.Model):
-    comment_id = models.IntegerField()
-    user = models.ForeignKey(User)
+    comment = models.ForeignKey(Comment)
+    reader = models.ForeignKey(User)
     rating = models.IntegerField() # -100..100
 
     class Meta:
