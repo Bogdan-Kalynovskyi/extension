878e2ec9f5514aea5e70a8362b211dfafe5cb8ee
easy-one <mybodya@gmail.com>
2014-01-30 13:50:41 +0100

    frontend:  working prototype with hover

--- extension/content.js
+++ extension/content.js
@@ -5,9 +5,9 @@
     var host = 'http://192.168.1.72:8080/api/rating/',
         userName = getLoggedUserName(),
         author = location.host.substr(0, location.host.indexOf('.')),
-        postId = parseInt(location.pathname.substr(1));
-
-    var width = 200;
+        postId = parseInt(location.pathname.substr(1)),
+        ratings = {},
+        width = 200;
 
     function htmlEntities(str) {
         return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
@@ -25,18 +25,11 @@
     }
 
 
-    function getEventRelativeX(el, event) {
-        var elementData = el.getBoundingClientRect();
-    	
-    	return event.pageX - elementData.left;
-    }
-
-
     function doRate(event) {
         var el = this,
             url = host + 'set/',
             commentId = parseInt(el.parentNode.id.substr(1)),
-            clickX = getEventRelativeX(el, event),
+            clickX = getEventRelativeX(event),
             rating = (clickX - width/2) / width/2 * 100,
             data = {
                     userName: userName,
@@ -49,6 +42,8 @@
         ajax.get(url, data, function() {
                 //
         });
+
+        return false;
     }
 
 
@@ -59,7 +54,7 @@
             clone,
             hover,
             comment,
-            commentId
+            commentId,
             rating;
 
 		ratingBarPrototype.innerHTML =  '<div class="rating">' +
@@ -72,32 +67,37 @@
 
             comment = comments[i];
             commentId = parseInt(comment.parentNode.id.substr(1)),
-            rating = ratings[id];
+            rating = ratings[commentId];
             if (rating !== undefined) {
 				clone.childNodes[0].style.background = 'red';
             	clone.childNodes[0].childNodes[0].style.left = (rating - 100) / 200 * width + 'px';
             }
-        	hover = clone.childNodes[0].childNodes[1];
-
             clone.addEventListener('click', doRate);
-            clone.addEventListener('mouseenter', function() {
-            	hover.style.display = 'block';
-            });
-            clone.addEventListener('mouseleave', function() {
-            	hover.style.display = 'none';
-            });
-            clone.addEventListener('mousemove', function(event) {
-            	eventX = getEventRelativeX(hover, event),
-            	hover.style.left = (eventX - 10) + 'px';
-            });
 
-            comment.appendChild(clone);
+            (function() {
+	        	var hover = clone.childNodes[0].childNodes[1],
+	        		container = clone;
+
+	            clone.addEventListener('mouseenter', function() {
+	            	hover.style.display = 'block';
+	            });
+	            clone.addEventListener('mouseleave', function() {
+	            	hover.style.display = 'none';
+	            });
+	            clone.addEventListener('mousemove', function(event) {
+	                var elementPos = container.getBoundingClientRect(),
+						eventX = event.clientX - elementPos.left;
+	            	hover.style.left = (eventX - 12) + 'px';
+	            });
+
+	            comment.appendChild(clone);
+	        })();
         }
 
     }
 
 
-    createRatingBoxes();
+    setTimeout(createRatingBoxes, 2000);
 
     ajax.get(host, {
             author: author,
